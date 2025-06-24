c0e75c4ae82d446a7d2a564eae8057a17abb92db
Matias Herklotz <matias.herklotz@globalhitss.com.br>
2025-06-24 19:40:54 -0300

    .

---
+++ api/repository/blog_post_repository.py
@@ -0,0 +1,71 @@
+from typing import Dict, Optional
+from datetime import datetime
+from api.errors.blog_post_errors import PostNotFoundError
+from api.models.blog_post_models import BlogPost, BlogPostCreate, BlogPostPatch, BlogPostUpdate
+
+class BlogPostRepository():
+    def __init__(self) -> None:
+        self.db: Dict[int, BlogPost] = {}
+        
+    def _get_next_id(self) -> int:
+        return max(list(self.db.keys())) + 1 if self.db != {} else 1
+    
+    def create_blog_post(self, blog_post: BlogPostCreate) -> BlogPost:   
+        blog_post = BlogPost(
+            post_id=self._get_next_id(),
+            title=blog_post.title,
+            content=blog_post.content,
+            author=blog_post.author,
+            created_at=datetime.now(),
+            updated_at=datetime.now()
+        )        
+        self.db[blog_post.post_id] = blog_post
+        return blog_post
+    
+    def update_blog_post(self, post_id:int, blog_post:BlogPostUpdate) -> BlogPost:
+        if post_id not in self.db:
+            raise PostNotFoundError(post_id)
+        
+        blog_post = BlogPost(
+            post_id=post_id,
+            title=blog_post.title,
+            content=blog_post.content,
+            author=blog_post.author,
+            created_at=self.db[post_id].created_at,
+            updated_at=datetime.now()
+        )
+        
+        self.db[post_id] = blog_post
+        return blog_post
+    
+    def patch_blog_post(self, post_id:int, blog_post:BlogPostPatch) -> BlogPost:
+        if post_id not in self.db:
+            raise PostNotFoundError(post_id)
+        
+        self.db[post_id].updated_at = datetime.now()
+        
+        if blog_post.title is not None:
+            self.db[post_id].title = blog_post.title
+            
+        if blog_post.author is not None:
+            self.db[post_id].author = blog_post.author
+            
+        if blog_post.content is not None:
+            self.db[post_id].content = blog_post.content
+        
+        return self.db[post_id]
+    
+    def delete_blog_post(self, post_id:int) -> int:
+        if post_id not in self.db:
+            raise PostNotFoundError(post_id)
+        
+        self.db.pop(post_id)
+        return post_id
+    
+    def get_blog_post(self, post_id:int) -> Optional[BlogPost]:
+        if post_id not in self.db:
+            raise PostNotFoundError(post_id)
+        return self.db[post_id]
+    
+    def get_all_blog_posts(self) -> List[BlogPost]:
+        return list(self.db.values())
--- api/services/blog_post_services.py
+++ api/services/blog_post_services.py
@@ -1,74 +1,25 @@
-from datetime import datetime
-from typing import Dict, List, Optional
+from typing import List, Optional
 from api.models.blog_post_models import BlogPost, BlogPostCreate, BlogPostUpdate, BlogPostPatch
-from api.errors.blog_post_errors import PostAlreadyExists, PostNotFoundError
-
-db: Dict[int, BlogPost] = {}
+from api.repository.blog_post_repository import BlogPostRepository
 
 class BlogPostService():
     def __init__(self) -> None:
-        pass
+        self.repository = BlogPostRepository()
     
     def create_blog_post(self, blog_post:BlogPostCreate) -> BlogPost:       
-        next_id = max(list(db.keys())) + 1 if db != {} else 1
-        current_datetime = datetime.now()
-        
-        blog_post = BlogPost(
-            post_id=next_id,
-            title=blog_post.title,
-            content=blog_post.content,
-            author=blog_post.author,
-            created_at=current_datetime,
-            updated_at=current_datetime
-        )        
-        db[blog_post.post_id] = blog_post
-        return blog_post
+        return self.repository.create_blog_post(blog_post)
     
     def update_blog_post(self, post_id:int, blog_post:BlogPostUpdate) -> BlogPost:
-        if post_id not in db:
-            raise PostNotFoundError(post_id)
-        
-        current_datetime = datetime.now()
-        
-        blog_post = BlogPost(
-            post_id=post_id,
-            title=blog_post.title,
-            content=blog_post.content,
-            author=blog_post.author,
-            created_at=db[post_id].created_at,
-            updated_at=current_datetime
-        )
-        
-        db[post_id] = blog_post
-        return blog_post
+        return self.repository.update_blog_post(post_id, blog_post)
     
     def patch_blog_post(self, post_id:int, blog_post:BlogPostPatch) -> BlogPost:
-        if post_id not in db:
-            raise PostNotFoundError(post_id)
-        
-        db[post_id].updated_at = datetime.now()
-        
-        if blog_post.title is not None:
-            db[post_id].title = blog_post.title
-            
-        if blog_post.author is not None:
-            db[post_id].author = blog_post.author
-            
-        if blog_post.content is not None:
-            db[post_id].content = blog_post.content
-        
-        return db[post_id]
+        return self.repository.patch_blog_post(post_id, blog_post)
         
     def delete_blog_post(self, post_id:int) -> int:
-        if post_id not in db:
-            raise PostNotFoundError(post_id)
-        db.pop(post_id) 
-        return post_id
+        return self.repository.delete_blog_post(post_id)
     
     def get_blog_post(self, post_id:int) -> Optional[BlogPost]:
-        if post_id not in db:
-            raise PostNotFoundError(post_id)
-        return db.get(post_id, None)
+        return self.repository.get_blog_post(post_id)
     
     def get_all_blog_posts(self) -> List[BlogPost]:
-        return list(db.values())
+        return self.repository.get_all_blog_posts()
